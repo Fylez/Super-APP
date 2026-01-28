2d767febf87829ddd46cd5b9a9b0681e14c4893c
Fylez <manu791_tenis@hotmail.com>
2026-01-28 14:46:42 +0100

    Updated package.json

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./types.ts",
+    "./constants.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
